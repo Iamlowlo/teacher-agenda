d71e51201f44e19b43b3b9fd2eb2123cb7ab9d36
Carlos Lopez Perez <carlos.lopez-perez@lbigroup.es>
2015-11-27 16:59:42 +0100

    emberfire working, but changes 0 value for a false and returns all the elements

--- app/routes/classunits.js
+++ app/routes/classunits.js
@@ -2,10 +2,6 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
 	model(){
-    return this.store.query('classunit', { orderBy: 'center', equalTo: 'creativa' } );
-		return Ember.RSVP.hash({
-      mon: this.store.query('classunit', { day: '0' } ),
-      tue: this.store.query('alumn',{ day : 1})
-    });
+    return this.store.query('classunit', { orderBy: 'day', equalTo: 4 } );
 	}
 });
