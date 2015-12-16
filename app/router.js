1ef1c9d0e44e375e2c1dcf658f8dcdedabc298ed
Carlos Lopez Perez <carlos.lopez-perez@lbigroup.es>
2015-12-16 11:14:21 +0100

    added new route class-info

--- app/router.js
+++ app/router.js
@@ -13,6 +13,7 @@ Router.map(function() {
   this.route('add-alumn');
   this.route('add-center');
   this.route('add-classunit');
+  this.route('class-info', {path: '/class-info/:id'});
 });
 
 export default Router;
