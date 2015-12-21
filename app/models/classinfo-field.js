02c1f83f155fd599b3a8e91e142182cda5f89783
Carlos Lopez Perez <iamlowlo@gmail.com>
2015-12-21 11:18:16 +0100

    classinfo-fields model created

--- app/models/classinfo-field.js
+++
@@ -1,8 +0,0 @@
-import DS from 'ember-data';
-
-export default DS.Model.extend({
-  name : DS.attr('string'),
-  checked : DS.attr('boolean'),
-  content : DS.attr('string'),
-  classinfo : DS.belongsTo('classinfo', { async: true })
-});
--- app/models/classinfo.js
+++ app/models/classinfo.js
@@ -3,5 +3,5 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   date : DS.attr('number'),
   classunit : DS.belongsTo('classunit', { async: true }),
-  classinfofield : DS.hasMany('classinfo-field', { async: true })
+  classinfofield : DS.hasMany('classinfofield', { async: true })
 });
