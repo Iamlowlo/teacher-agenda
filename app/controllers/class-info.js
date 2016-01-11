0e0e8821a0d522f78936c78775f0bc051e1c712f
Carlos Lopez Perez <carlos.lopez-perez@lbigroup.es>
2016-01-11 17:56:51 +0100

    class info objects on creation

--- app/controllers/class-info.js
+++ app/controllers/class-info.js
@@ -1,14 +1,29 @@
 import Ember from 'ember';
 
+let Input = Ember.Object.extend({
+	checked : false,
+	content : '',
+	name : ''
+})
+
 export default Ember.Controller.extend({
 	isCreationMode : false,
   classTag: '',
+  classField: '',
+  inputFieldList: [],
 	actions: {
 		openCreationMode : function(){
 			this.toggleProperty('isCreationMode');
+			this.set('date','');
+			let topics = this.get('model.classtype.topics');
+			for (var i = 0; i < topics.length; i++) {
+				let inputField = Input.create();
+				inputField.set('name',topics[i]);
+				this.get('inputFieldList').push(inputField);
+			};
 		},
 		addClassInfo : function(){
-			console.log('addClassInfo');
+			console.log(this.get('inputFieldList'));
 		}
 	}
 });
