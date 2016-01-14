0ba8e4411edac531ec525e69e1e654a0eb35637e
Carlos Lopez Perez <carlos.lopez-perez@lbigroup.es>
2016-01-14 17:41:50 +0100

    Added or helper to if blocks

---
+++ app/helpers/or.js
@@ -0,0 +1,11 @@
+import Ember from 'ember';
+
+export function and(params) {
+  let result = false;
+  for (var i = 0; i < params.length; i++) {
+    result = result || !!params[i]
+  };
+  return result;
+}
+
+export default Ember.Helper.helper(and);
